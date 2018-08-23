932491834f2b9b2081c6cea5e0cf3d0bb90b52a5
wkwksama <mailpakgasing@gmail.com>
2018-08-23 15:36:37 +0700

    style hero color and background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import { Jumbotron, Button } from 'reactstrap';
 class Hero extends  React.Component {
   render () {
     return (
-      <Jumbotron className="text-center">
+      <Jumbotron className="text-center" style={{ background: 'url(http://i66.tinypic.com/28am9fs.jpg)', color: 'white' }}>
         <h1 className="display-3">Belajar Jadi Asyik!</h1>
         <p className="lead">Kegiatan Belajar Intensif Programming dan Qur'an bersama Santren Koding.</p>
         <p className="lead">
-          <Button color="success">Mulai Belajar</Button>
+          <Button color="success" size="lg">Mulai Belajar</Button>
         </p>
       </Jumbotron>
     )
